1ef2463d87d5333a5b5940b39e466cd81b9d9d38
georgeburt <georgeburt@hotmail.com>
2021-12-10 22:05:16 +0000

    Ammo Trigger logic [incomplete]

---
+++ ESPER/Assets/ammoTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ammoTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Debug.Log("COLLIDED");
+            Destroy(gameObject);
+        }
+    }
+}
